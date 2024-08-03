0a8e1283a8efb743c55b0726b0bbbfe7941584a0
JoanMburu <joan.wangui@student.moringaschool.com>
2024-08-03 22:09:51 +0300

    tree traversal

--- lib/tree.py
+++ lib/tree.py
@@ -1,6 +1,19 @@
-class Tree:
-  def __init__(self, root = None):
-    self.root = root
+from collections import deque
 
-  def get_element_by_id(self, id):
-    pass
+class Tree:
+    def __init__(self, root=None):
+        self.root = root
+
+    def get_element_by_id(self, id):
+        # Use a queue for breadth-first traversal
+        queue = deque([self.root])
+        
+        while queue:
+            node = queue.popleft()
+            if node is None:
+                continue
+            if node.get('id') == id:
+                return node
+            queue.extend(node.get('children', []))
+        
+        return None
